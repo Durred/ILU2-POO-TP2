d26b524cce475c2ac07e73a0146f066026c3dd25
Durred <joshua.vigier@univ-tlse3.fr>
2024-04-03 11:13:11 +0200

    BugFix

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -9,27 +9,34 @@ public class ControlAcheterProduit {
 	private ControlVerifierIdentite controlVerifierIdentite;
 
 	public ControlAcheterProduit(ControlVerifierIdentite controlVerifierIdentite,
-			ControlTrouverEtalVendeur controlTrouverEtalVendeur,
-			Village village) {
+			ControlTrouverEtalVendeur controlTrouverEtalVendeur, Village village) {
 		this.village = village;
 		this.controlVerifierIdentite = controlVerifierIdentite;
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
-	
+
 	public String[] trouverVendeursProduit(String produit) {
 		Gaulois[] gaulois = village.rechercherVendeursProduit(produit);
 		String[] vendeursProduits = new String[gaulois.length];
-		for(int i = 0; i<gaulois.length;i++) {
-			vendeursProduits[i] = gaulois[i].getNom();
+		if (gaulois.length != 0) {
+			for (int i = 0; i < gaulois.length; i++) {
+				vendeursProduits[i] = gaulois[i].getNom();
+			}
 		}
 		return vendeursProduits;
 	}
-	
+
 	public int getNombreProduit(String vendeur) {
 		return village.rechercherEtal(village.trouverHabitant(vendeur)).getQuantite();
 	}
-	
+
 	public void acheterProduit(int quantity, String vendeur) {
-		return controlTrouverEtalVendeur.trouverEtalVendeur(vendeur).acheterProduit(quantity);
+		if (controlVerifierIdentite.verifierIdentite(vendeur)) {
+			controlTrouverEtalVendeur.trouverEtalVendeur(vendeur).acheterProduit(quantity);
+		}
+	}
+
+	public Boolean isAcheteurFromVillage(String nom) {
+		return controlVerifierIdentite.verifierIdentite(nom);
 	}
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -13,39 +13,46 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		System.out.println("Quel produit voulez-vous acheter ?\n");
-		String produit = scan.nextLine();
-		String[] vendeursProduit = controlAcheterProduit.trouverVendeursProduit(produit);
-		if(vendeursProduit == null) {
-			System.out.println("D�sol�, personne ne vend ce produit au march�.\n");
-		}
-		else {
-			System.out.println("Chez quel commer�ant voulez-vous acheter des "+produit+" ?\n");
-			for(int i = 1; i<vendeursProduit.length; i++) {
-				System.out.println(i+" - "+vendeursProduit[i-1]+"\n");
-			}
-			int nEtal = scan.nextInt();
-			if (nEtal>= vendeursProduit.length || nEtal < 0) {
-				System.out.println("Il n'y a pas votre produit � cette �tal.");
+		if (controlAcheterProduit.isAcheteurFromVillage(nomAcheteur)) {
+			System.out.println("Quel produit voulez-vous acheter ?\n");
+			String produit = scan.nextLine();
+			String[] vendeursProduit = controlAcheterProduit.trouverVendeursProduit(produit);
+			if (vendeursProduit.length == 0) {
+				System.out.println("D�sol�, personne ne vend ce produit au march�.\n");
 			} else {
-				System.out.println(nomAcheteur+" se d�place jusqu'� l'�tal du vendeur " + vendeursProduit[nEtal]);
-				System.out.println("Combien de "+produit +" voulez-vous acheter ?\n");
-				int quantityToBuy = scan.nextInt();
-				int quantityAvailable = controlAcheterProduit.getNombreProduit(vendeursProduit[nEtal]);
-				if (quantityAvailable>=quantityToBuy) {
-					System.out.println(nomAcheteur+" ach�te " + quantityToBuy + " "+ produit+" � "+ vendeursProduit[nEtal]+".\n");
-					controlAcheterProduit.acheterProduit(vendeursProduit[nEtal], quantityToBuy);
+				System.out.println("Chez quel commer�ant voulez-vous acheter des " + produit + " ?\n");
+				for (int i = 0; i < vendeursProduit.length; i++) {
+					System.out.println((i+1) + " - " + vendeursProduit[i] + "\n");
+				}
+				int nEtal = scan.nextInt()-1;
+				if (nEtal > vendeursProduit.length || nEtal < 0) {
+					System.out.println("Il n'y a pas votre produit � cette �tal.");
 				} else {
-					if (quantityAvailable<quantityToBuy && quantityAvailable<=0 ) {
-						System.out.println(nomAcheteur + " veut acheter "+quantityToBuy +" "+ produit + ", malheureusement il n'y en a plus !\n");
+					System.out.println(
+							nomAcheteur + " se d�place jusqu'� l'�tal du vendeur " + vendeursProduit[nEtal]);
+					System.out.println("Combien de " + produit + " voulez-vous acheter ?\n");
+					int quantityToBuy = scan.nextInt();
+					int quantityAvailable = controlAcheterProduit.getNombreProduit(vendeursProduit[nEtal]);
+					if (quantityAvailable >= quantityToBuy) {
+						System.out.println(nomAcheteur + " ach�te " + quantityToBuy + " " + produit + " � "
+								+ vendeursProduit[nEtal] + ".\n");
+						controlAcheterProduit.acheterProduit(quantityToBuy, vendeursProduit[nEtal]);
 					} else {
-						System.out.println(nomAcheteur + " veut acheter "+quantityToBuy +" "+ produit + ", malheureusement "+ vendeursProduit[nEtal]+" n'en a plus que "
-						+ quantityAvailable + ". " + nomAcheteur + " achète tout le stock de " + vendeursProduit[nEtal]+"\n");
-						controlAcheterProduit.acheterProduit(vendeursProduit[nEtal], quantityAvailable);
+						if (quantityAvailable < quantityToBuy && quantityAvailable <= 0) {
+							System.out.println(nomAcheteur + " veut acheter " + quantityToBuy + " " + produit
+									+ ", malheureusement il n'y en a plus !\n");
+						} else {
+							System.out.println(nomAcheteur + " veut acheter " + quantityToBuy + " " + produit
+									+ ", malheureusement " + vendeursProduit[nEtal] + " n'en a plus que "
+									+ quantityAvailable + ". " + nomAcheteur + " achète tout le stock de "
+									+ vendeursProduit[nEtal] + "\n");
+							controlAcheterProduit.acheterProduit(quantityAvailable, vendeursProduit[nEtal]);
+						}
 					}
 				}
-				
-			}		
+			}
+		} else {
+			System.out.println("Il faut faire partie du village pour acheter des produits\n");
 		}
 	}
 }
